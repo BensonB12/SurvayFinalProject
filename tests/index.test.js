972d38f886a13c1532cbf8f879e3225b7711b72c
Benson Bird <benson@ENGR5HCP5S3.ad.snow.edu>
2023-12-01 15:01:50 -0700

    IDK

---
+++ tests/index.test.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../src/index");
+// describe('testing index file', () => {
+//   test('empty string should result in zero', () => {
+//     expect(add('')).toBe(0);
+//   });
+// });
+describe('testing index file', () => {
+    test('InterpilatingStrings works, should be Hello World', () => {
+        expect((0, index_1.interprilatingStrings)()).toBe('Hello World!');
+    });
+});
+describe('testing index file', () => {
+    test('a = 3, b = 2, c = 1 should be 18', () => {
+        expect((0, index_1.advancedMathmatics)(3, 2, 1)).toBe(18);
+    });
+});
